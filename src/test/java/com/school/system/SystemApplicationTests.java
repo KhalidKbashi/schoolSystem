a2b880cbd90d500fd152af8a3f0ce1c0b5cad0fa
Khalid Kbashi <TechnoMaster201@gmail.com>
2022-02-28 09:32:45 +0200

    where everything started

---
+++ src/test/java/com/school/system/SystemApplicationTests.java
@@ -0,0 +1,15 @@
+package com.school.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
