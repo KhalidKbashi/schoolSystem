d610dd43e7fc84b470e0555f9b66456443e59191
Khalid Kbashi <TechnoMaster201@gmail.com>
2022-02-28 14:13:51 +0200

    student, teacher, subject service is done !

--- src/main/java/com/school/system/services/subjectService.java
+++ src/main/java/com/school/system/services/subjectService.java
@@ -1,8 +1,77 @@
 package com.school.system.services;
 
+import com.school.system.entities.subject;
+import com.school.system.repos.subjectRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
-public class subjectService
+public class subjectService implements templateService<subject,subjectRepo>
 {
+    private subjectRepo subjectRepo;
+
+    @Autowired
+    public subjectService(subjectRepo subjectRepo)
+    {
+        this.subjectRepo = subjectRepo;
+    }
+
+
+
+    @Override
+    public UUID add(subject subject)
+    {
+        return this.subjectRepo.save(subject).getId();
+    }
+
+    @Override
+    public subject get(UUID id)
+    {
+        Optional<subject> temp = this.subjectRepo.findById(id);
+        return temp.orElse(null);
+    }
+
+    @Override
+    public Object[] getAll()
+    {
+        Collection temp = (Collection) this.subjectRepo.findAll();
+        return temp.toArray();
+    }
+
+    @Override
+    public void update(subject temp)
+    {
+        this.subjectRepo.save(temp);
+    }
+
+    @Override
+    public void patchUpdate(subject temp, UUID id)
+    {
+        Optional<subject> target = this.subjectRepo.findById(id);
+        if(target.isPresent())
+        {
+            if(Objects.nonNull(temp.getName()))
+                target.get().setName(temp.getName());
+            if(temp.getNumOfHours()!=0)
+                target.get().setNumOfHours(temp.getNumOfHours());
+        }
+    }
+
+    @Override
+    public void delete(UUID id)
+    {
+        if(check(id))
+            this.subjectRepo.deleteById(id);
+    }
+
+    @Override
+    public boolean check(UUID id)
+    {
+        return this.subjectRepo.existsById(id);
+    }
 }
--- src/main/java/com/school/system/services/teacherService.java
+++ src/main/java/com/school/system/services/teacherService.java
@@ -1,8 +1,84 @@
 package com.school.system.services;
 
+import com.school.system.entities.teacher;
+import com.school.system.repos.teacherRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
-public class teacherService
+public class teacherService implements templateService<teacher, teacherRepo>
 {
+    private teacherRepo teacherRepo;
+
+    @Autowired
+    public teacherService(teacherRepo teacherRepo)
+    {
+        this.teacherRepo = teacherRepo;
+    }
+
+
+
+    @Override
+    public UUID add(teacher teacher)
+    {
+        return this.teacherRepo.save(teacher).getId();
+    }
+
+    @Override
+    public teacher get(UUID id)
+    {
+        Optional<teacher> temp = this.teacherRepo.findById(id);
+        return temp.orElse(null);
+    }
+
+    @Override
+    public Object[] getAll()
+    {
+        Collection temp = (Collection) this.teacherRepo.findAll();
+        return temp.toArray();
+    }
+
+    @Override
+    public void update(teacher temp)
+    {
+        this.teacherRepo.save(temp);
+    }
+
+    @Override
+    public void patchUpdate(teacher temp, UUID id)
+    {
+        Optional<teacher> target = teacherRepo.findById(id);
+        if(target.isPresent())
+        {
+            if(Objects.nonNull(temp.getName()))
+                target.get().setName(temp.getName());
+
+            if(temp.getAge()!=0)
+                target.get().setAge(temp.getAge());
+
+            if(Objects.nonNull(temp.getSubject()))
+                target.get().setSubject(temp.getSubject());
+
+            if(temp.getYearsOfExperience()!=0)
+                target.get().setYearsOfExperience(temp.getYearsOfExperience());
+        }
+    }
+
+    @Override
+    public void delete(UUID id)
+    {
+        if(check(id))
+            this.teacherRepo.deleteById(id);
+    }
+
+    @Override
+    public boolean check(UUID id)
+    {
+        return this.teacherRepo.existsById(id);
+    }
 }
