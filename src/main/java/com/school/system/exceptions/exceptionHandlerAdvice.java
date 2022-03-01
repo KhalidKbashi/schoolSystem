07e80a79a1230ab4e3fe6d45599357b493329f40
Khalid Kbashi <TechnoMaster201@gmail.com>
2022-03-01 23:02:22 +0200

    nothing more

--- src/main/java/com/school/system/exceptions/exceptionHandlerAdvice.java
+++ src/main/java/com/school/system/exceptions/exceptionHandlerAdvice.java
@@ -13,69 +13,74 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
-
 @ControllerAdvice
 public class exceptionHandlerAdvice extends ResponseEntityExceptionHandler
 {
-
+    //todo Allign Error Response paylod
+    //NOT_ACCEPTABLE
     @ExceptionHandler(NotAcceptableDataException.class)
     public ResponseEntity<ErrorResponse> notAcceptableDataExceptionHandler(NotAcceptableDataException ex,
-            WebRequest webRequest)
+            WebRequest request)
     {
         ErrorResponse errorResponse = new ErrorResponse(
                 ex.getMessage()
                 , HttpStatus.NOT_ACCEPTABLE
                 ,ex.getLocalizedMessage()
-                , webRequest.getDescription(false)
+                , request.getDescription(false)
                 , System.currentTimeMillis());
         return new ResponseEntity(errorResponse, errorResponse.getErrorCode());
     }
-
+    //NOT_FOUND
     @ExceptionHandler(RecordNotFoundException.class)
     public ResponseEntity<ErrorResponse> recordNotFoundExceptionHandler(RecordNotFoundException ex,
-            WebRequest webRequest)
+            WebRequest request)
     {
         ErrorResponse errorResponse = new ErrorResponse(
                 ex.getMessage()
                 , HttpStatus.NOT_FOUND
                 ,ex.getLocalizedMessage()
-                , webRequest.getDescription(false)
+                , request.getDescription(false)
                 , System.currentTimeMillis());
         return new ResponseEntity(errorResponse, errorResponse.getErrorCode());
     }
-
-    /*@ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> globalExceptionHandler(MethodArgumentNotValidException ex,
-            HttpServletRequest webRequest)
-    {
-        ErrorResponse errorResponse = new ErrorResponse("Something missing", HttpStatus.NOT_ACCEPTABLE,
-                webRequest.getRequestURI(), System.currentTimeMillis());
-        return new ResponseEntity(errorResponse, errorResponse.getErrorCode());
-    }*/
-
+    //NOT_ACCEPTABLE
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex
-            , HttpHeaders headers, HttpStatus status, WebRequest webRequest)
+            , HttpHeaders headers, HttpStatus status, WebRequest request)
     {
         ErrorResponse errorResponse = new ErrorResponse(
                 "Validation Error Detected"
                 , HttpStatus.NOT_ACCEPTABLE
                 ,ex.getLocalizedMessage()
-                , webRequest.getDescription(false)
+                , request.getDescription(false)
                 , System.currentTimeMillis());
         return new ResponseEntity(errorResponse, errorResponse.getErrorCode());
     }
-
+    //RESET_CONTENT
     @Override
-    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+    protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex
+            , HttpHeaders headers, HttpStatus status, WebRequest request)
     {
-        return super.handleMissingPathVariable(ex, headers, status, request);
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Missing Path Variable"
+                , HttpStatus.RESET_CONTENT
+                ,ex.getLocalizedMessage()
+                , request.getDescription(false)
+                , System.currentTimeMillis());
+        return new ResponseEntity(errorResponse, errorResponse.getErrorCode());
     }
-
+    //INTERNAL_SERVER_ERROR
     @Override
-    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex
+            , HttpHeaders headers, HttpStatus status, WebRequest request)
     {
-        return super.handleNoHandlerFoundException(ex, headers, status, request);
+        ErrorResponse errorResponse = new ErrorResponse(
+                "Something Gone Wrong"
+                , HttpStatus.INTERNAL_SERVER_ERROR
+                ,ex.getLocalizedMessage()
+                , request.getDescription(false)
+                , System.currentTimeMillis());
+        return new ResponseEntity(errorResponse, errorResponse.getErrorCode());
     }
 }
